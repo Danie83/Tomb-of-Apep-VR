6ffc23b33a820d86a6169b49bf346f59cbbe41a8
Paul <gigi.boss52@gmail.com>
2021-12-09 23:19:13 +0200

    update

---
+++ Tomb Of Apep VR/Assets/Character/Scripts/EnemyWalk.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyWalk : MonoBehaviour
+{
+    [SerializeField]
+    private int dificulty = 1;
+    [SerializeField]
+    private bool isStunned = false;
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        switch (dificulty)
+        {
+            case 1:
+                agent.acceleration = 20;
+                agent.speed = 1;
+                agent.angularSpeed = 300;
+                break;
+            case 2:
+                agent.acceleration = 20;
+                agent.speed = 2;
+                agent.angularSpeed = 300;
+                break;
+            case 3:
+                agent.acceleration = 20;
+                agent.speed = 2.5f;
+                agent.angularSpeed = 300;
+                break;
+            default:
+                agent.acceleration = 20;
+                agent.speed = 1;
+                agent.angularSpeed = 300;
+                break;
+        }
+    }
+
+    public void MoveToLocation(Vector3 targetPoint)
+    {
+        if (!isStunned)
+        {
+            agent.destination = targetPoint;
+            agent.isStopped = false;
+        }
+    }
+
+    public void SetStun(bool stun)
+    {
+        isStunned = stun;
+        animator.SetBool("IsStunned", stun);
+    }
+}
