574b78e917e5b99a358b6be5ac3a77705697bc2f
Stefan Pezamosca <stefan2014pe@gmail.com>
2022-01-07 16:05:12 +0200

    audio

--- Tomb Of Apep VR/Assets/Scripts/Teleportation.cs
+++ Tomb Of Apep VR/Assets/Scripts/Teleportation.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleportation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OpenScene(string name)
     {
-        
+        SceneManager.LoadScene(name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenScene(int index)
     {
-        
+        SceneManager.LoadScene(index);
     }
 }
